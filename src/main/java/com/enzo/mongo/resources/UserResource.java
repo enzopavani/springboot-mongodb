5a314072a3698d97f355a047473eb89fa2bf409f
Enzo <tijoloenso@gmail.com>
2025-05-06 13:33:57 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/enzo/mongo/resources/UserResource.java
+++ src/main/java/com/enzo/mongo/resources/UserResource.java
@@ -1,25 +1,26 @@
 package com.enzo.mongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.enzo.mongo.domain.User;
+import com.enzo.mongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User enzo = new User("1", "Enzo", "enzo@mail.br");
-		User lara = new User("2", "Lara", "lara@mail.br");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(enzo, lara));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
