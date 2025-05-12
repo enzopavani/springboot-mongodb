16a5f029434a32fc05246e6bd3d144014f81a324
Enzo <tijoloenso@gmail.com>
2025-05-12 14:03:33 -0300

    Created Post entity with nested User

--- src/main/java/com/enzo/mongo/config/Instantiation.java
+++ src/main/java/com/enzo/mongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.enzo.mongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.enzo.mongo.domain.Post;
 import com.enzo.mongo.domain.User;
+import com.enzo.mongo.repositories.PostRepository;
 import com.enzo.mongo.repositories.UserRepository;
 
 @Configuration
@@ -15,16 +19,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("03/05/2025"), "Lady Gaga concert", "Going to the beach in Rio de Janeiro with over 2 million people.", maria);
+		Post post2 = new Post(null, sdf.parse("08/05/2025"), "SOAD concert", "Not even a week since the last concert, my throat is sore. ;-;", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
 }
