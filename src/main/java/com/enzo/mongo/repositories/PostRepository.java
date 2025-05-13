b69633f34ad25809caa72dc98dfaf999e4380bf5
Enzo <tijoloenso@gmail.com>
2025-05-13 15:24:30 -0300

    Simple consultation with query methods

--- src/main/java/com/enzo/mongo/repositories/PostRepository.java
+++ src/main/java/com/enzo/mongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.enzo.mongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,9 @@ import com.enzo.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	// 6.3. Query methods
+	// https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
