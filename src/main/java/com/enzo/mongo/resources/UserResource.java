2e392eaaa83cd9a7e9666f16f8557bf859307e0e
Enzo <tijoloenso@gmail.com>
2025-05-13 14:41:54 -0300

    Endpoint to user posts

--- src/main/java/com/enzo/mongo/resources/UserResource.java
+++ src/main/java/com/enzo/mongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.enzo.mongo.domain.Post;
 import com.enzo.mongo.domain.User;
 import com.enzo.mongo.dto.UserDTO;
 import com.enzo.mongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
